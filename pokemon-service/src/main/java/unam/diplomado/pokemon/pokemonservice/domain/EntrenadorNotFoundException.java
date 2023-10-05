ce184d272bb1297d90311e72401e76bbd9bb3d5b
ae-de <67604088+ae-de@users.noreply.github.com>
2023-10-04 21:52:14 -0600

    Delete pokemon-service directory

--- pokemon-service/src/main/java/unam/diplomado/pokemon/pokemonservice/domain/EntrenadorNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package unam.diplomado.pokemon.pokemonservice.domain;
-
-public class EntrenadorNotFoundException extends RuntimeException {
-
-    public EntrenadorNotFoundException(String entrenadores) {
-        super( "El entrenador ya ha sido registrado" + entrenadores);
-    }
-}
--- pokemon-service/src/main/java/unam/diplomado/pokemon/pokemonservice/domain/PokemonNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package unam.diplomado.pokemon.pokemonservice.domain;
-
-public class PokemonNotFoundException extends RuntimeException {
-
-    public PokemonNotFoundException(String message) {
-        super(message);
-    }
-}
